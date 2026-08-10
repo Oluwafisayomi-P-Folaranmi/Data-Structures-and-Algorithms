d58ae65265daa52e08be8f71169333cba96af460
Oluwafisayomi-P-Folaranmi <folaranmfisayomi@gmail.com>
2026-08-10 13:13:23 +0100

    Yes

--- 0x04-Solving_Problems_with_SQDs/raauastack/src/Main.java
+++ 0x04-Solving_Problems_with_SQDs/raauastack/src/Main.java
@@ -1,7 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Stack.ArrayStack;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        Integer[] numbers = {1, 2, 3, 4, 5};
+        final int ARRAY_LENGTH = numbers.length;
+        System.out.println("Array before reversal -> " + Arrays.toString(numbers));
+
+        ArrayStack<Integer> integerArrayStack;
+        integerArrayStack = new ArrayStack<Integer>(5);
+
+        for (int index = 0; index < ARRAY_LENGTH; index++) {
+            integerArrayStack.push(numbers[index]);
+        }
+
+        // Reversing the array
+        for(int index = 0; index < ARRAY_LENGTH; index++) {
+            numbers[index] = integerArrayStack.pop();
+        }
+
+        System.out.println("Array after reversal -> " + Arrays.toString(numbers));
     }
 }
